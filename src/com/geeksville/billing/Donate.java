2fdb99ce973443ac470a2ce175bedba26e7aa12f
Kevin Hester <kevin@ridemission.com>
2011-12-28 21:13:28 -0800

    Add donate menu item

---
+++ src/com/geeksville/billing/Donate.java
@@ -0,0 +1,364 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.geeksville.billing;
+
+import com.geeksville.gaggle.R;
+import com.geeksville.billing.BillingService.RequestPurchase;
+import com.geeksville.billing.BillingService.RestoreTransactions;
+import com.geeksville.billing.Consts.PurchaseState;
+import com.geeksville.billing.Consts.ResponseCode;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.SpannableStringBuilder;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+/**
+ * A sample application that demonstrates in-app billing.
+ */
+public class Donate {
+  private static final String TAG = "Donate";
+
+  /**
+   * Used for storing the log text.
+   */
+  private static final String LOG_TEXT_KEY = "DUNGEONS_LOG_TEXT";
+
+  /**
+   * The SharedPreferences key for recording whether we initialized the
+   * database. If false, then we perform a RestoreTransactions request to get
+   * all the purchases for this user.
+   */
+  private static final String DB_INITIALIZED = "db_initialized";
+
+  private DonatePurchaseObserver mPurchaseObserver;
+  private Handler mHandler;
+
+  private BillingService mBillingService;
+  private PurchaseDatabase mPurchaseDatabase;
+  private Activity context;
+
+  /**
+   * The developer payload that is sent with subsequent purchase requests.
+   */
+  private String mPayloadContents = null;
+
+  /**
+   * Each product in the catalog is either MANAGED or UNMANAGED. MANAGED means
+   * that the product can be purchased only once per user (such as a new level
+   * in a game). The purchase is remembered by Android Market and can be
+   * restored if this application is uninstalled and then re-installed.
+   * UNMANAGED is used for products that can be used up and purchased multiple
+   * times (such as poker chips). It is up to the application to keep track of
+   * UNMANAGED products for the user.
+   */
+  private enum Managed {
+    MANAGED, UNMANAGED
+  }
+
+  /**
+   * A {@link PurchaseObserver} is used to get callbacks when Android Market
+   * sends messages to this application so that we can update the UI.
+   */
+  private class DonatePurchaseObserver extends PurchaseObserver {
+    public DonatePurchaseObserver(Handler handler) {
+      super(context, handler);
+    }
+
+    @Override
+    public void onBillingSupported(boolean supported) {
+      if (Consts.DEBUG) {
+        Log.i(TAG, "supported: " + supported);
+      }
+      if (supported) {
+        restoreDatabase();
+      }
+    }
+
+    @Override
+    public void onPurchaseStateChange(PurchaseState purchaseState,
+        String itemId, int quantity, long purchaseTime, String developerPayload) {
+      if (Consts.DEBUG) {
+        Log.i(TAG, "onPurchaseStateChange() itemId: " + itemId + " "
+            + purchaseState);
+      }
+
+      if (purchaseState == PurchaseState.PURCHASED) {
+        PreferenceManager.getDefaultSharedPreferences(context).edit()
+            .putBoolean("has_donated", true).commit();
+        thanksForDonating();
+      }
+
+      close(); // We don't need our service anymore
+    }
+
+    @Override
+    public void onRequestPurchaseResponse(RequestPurchase request,
+        ResponseCode responseCode) {
+      if (Consts.DEBUG) {
+        Log.d(TAG, request.mProductId + ": " + responseCode);
+      }
+      if (responseCode == ResponseCode.RESULT_OK) {
+        if (Consts.DEBUG) {
+          Log.i(TAG, "purchase was successfully sent to server");
+        }
+      } else if (responseCode == ResponseCode.RESULT_USER_CANCELED) {
+        if (Consts.DEBUG) {
+          Log.i(TAG, "user canceled purchase");
+        }
+      } else {
+        if (Consts.DEBUG) {
+          Log.i(TAG, "purchase failed");
+        }
+      }
+    }
+
+    @Override
+    public void onRestoreTransactionsResponse(RestoreTransactions request,
+        ResponseCode responseCode) {
+      if (responseCode == ResponseCode.RESULT_OK) {
+        if (Consts.DEBUG) {
+          Log.d(TAG, "completed RestoreTransactions request");
+        }
+        // Update the shared preferences so that we don't perform
+        // a RestoreTransactions again.
+        SharedPreferences prefs = context.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = prefs.edit();
+        edit.putBoolean(DB_INITIALIZED, true);
+        edit.commit();
+      } else {
+        if (Consts.DEBUG) {
+          Log.d(TAG, "RestoreTransactions error: " + responseCode);
+        }
+      }
+    }
+  }
+
+  private static class CatalogEntry {
+    public String sku;
+    public int nameId;
+    public Managed managed;
+
+    public CatalogEntry(String sku, int nameId, Managed managed) {
+      this.sku = sku;
+      this.nameId = nameId;
+      this.managed = managed;
+    }
+  }
+
+  /** An array of product list entries for the products that can be purchased. */
+  private static final CatalogEntry[] CATALOG = new CatalogEntry[] { new CatalogEntry(
+      "donate_basic", R.string.donation, Managed.MANAGED), };
+
+  private String mItemName;
+  private String mSku;
+
+  private boolean isBillingSupported;
+
+  private void promptToDonate() {
+    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+    builder
+        .setMessage(R.string.donate_text)
+        .setCancelable(false)
+        .setPositiveButton(R.string.yes_i_d_like_to_donate,
+            new DialogInterface.OnClickListener() {
+              public void onClick(DialogInterface dialog, int id) {
+                String sku = "donate_basic";
+                Log.d(TAG, "buying: " + sku);
+                dialog.cancel();
+                mBillingService.requestPurchase(sku, null);
+                // We don't call close, because we want to leave the server
+                // running a bit longer (to get the reply about this purchase)
+              }
+            })
+        .setNegativeButton(R.string.no_not_right_now,
+            new DialogInterface.OnClickListener() {
+              public void onClick(DialogInterface dialog, int id) {
+                close();
+                dialog.cancel();
+              }
+            });
+    AlertDialog alert = builder.create();
+    alert.show();
+  }
+
+  /**
+   * Has the user donated?
+   * 
+   * @return
+   */
+  private boolean isDonated() {
+
+    // Even though this is an open source app, don't turn off this check and
+    // release a version without
+    // this donation reminder. The small amount of totally optional donations I
+    // receive repays me for
+    // the huge amount of time I have invested. -kevin
+
+    return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(
+        "has_donated", false);
+  }
+
+  /**
+   * Is it time to show the nag screen?
+   * 
+   * @return
+   */
+  private boolean isPromptToUpdate() {
+
+    // Even though this is an open source app, don't turn off this check and
+    // release a version without
+    // this donation reminder. The small amount of totally optional donations I
+    // receive repays me for
+    // the huge amount of time I have invested. -kevin
+
+    if (!context.getString(R.string.should_prompt_to_donate).equals("true"))
+      return false;
+
+    SharedPreferences prefs = PreferenceManager
+        .getDefaultSharedPreferences(context);
+    long lastCheckDate = prefs.getLong("donate_check_date", -1);
+
+    long now = System.currentTimeMillis();
+    prefs.edit().putLong("donate_check_date", now).commit();
+
+    if (lastCheckDate == -1) // Don't nag the first time we launch
+      return false;
+
+    long span = now - lastCheckDate;
+    long checkInterval = 3 * 24 * 60 * 60 * 1000; // Only nag once every three
+                                                  // days
+    if (span < checkInterval)
+      return false;
+
+    // Check to see if the market server says we already have donated
+    Cursor c = mPurchaseDatabase.queryAllPurchasedItems();
+    int numPurchased = c.getCount();
+    c.close();
+
+    if (numPurchased > 0) {
+      PreferenceManager.getDefaultSharedPreferences(context).edit()
+          .putBoolean("has_donated", true).commit();
+      thanksForDonating();
+      return false;
+    }
+
+    return true;
+  }
+
+  private void thanksForDonating() {
+    // Even though this is an open source app, don't turn off this check and
+    // release a version without
+    // this donation reminder. The small amount of totally optional donations I
+    // receive repays me for
+    // the huge amount of time I have invested. -kevin
+
+    Toast.makeText(context, "Thank you for donating!", Toast.LENGTH_LONG)
+        .show();
+  }
+
+  public void perhapsSplash() {
+    // Even though this is an open source app, don't turn off this check and
+    // release a version without
+    // this donation reminder. The small amount of totally optional donations I
+    // receive repays me for
+    // the huge amount of time I have invested. -kevin
+
+    if (isDonated()) {
+      thanksForDonating();
+      close();
+    } else if (isPromptToUpdate())
+      promptToDonate();
+    else
+      close(); // Not bothering the user this time
+  }
+
+  public void splash() {
+    if (isDonated()) {
+      thanksForDonating();
+      close();
+    } else
+      promptToDonate();
+  }
+
+  public Donate(Activity context) {
+    this.context = context;
+
+    mHandler = new Handler();
+    mPurchaseObserver = new DonatePurchaseObserver(mHandler);
+
+    // If we've already donate no need to start the service
+    if (!isDonated()) {
+      mBillingService = new BillingService();
+      mBillingService.setContext(context);
+
+      mPurchaseDatabase = new PurchaseDatabase(context);
+
+      // Check if billing is supported.
+      ResponseHandler.register(mPurchaseObserver);
+      isBillingSupported = mBillingService.checkBillingSupported();
+    }
+  }
+
+  /** Release critical resources */
+  private void close() {
+
+    ResponseHandler.unregister(mPurchaseObserver);
+    if (mPurchaseDatabase != null)
+      mPurchaseDatabase.close();
+    if (mBillingService != null)
+      mBillingService.unbind();
+  }
+
+  /**
+   * If the database has not been initialized, we send a RESTORE_TRANSACTIONS
+   * request to Android Market to get the list of purchased items for this user.
+   * This happens if the application has just been installed or the user wiped
+   * data. We do not want to do this on every startup, rather, we want to do
+   * only when the database needs to be initialized.
+   */
+  private void restoreDatabase() {
+    SharedPreferences prefs = PreferenceManager
+        .getDefaultSharedPreferences(context);
+    boolean initialized = prefs.getBoolean(DB_INITIALIZED, false);
+    if (!initialized) {
+      mBillingService.restoreTransactions();
+      // Toast.makeText(context, R.string.restoring_transactions,
+      // Toast.LENGTH_LONG).show();
+    }
+  }
+
+}
--- src/com/geeksville/gaggle/TopActivity.java
+++ src/com/geeksville/gaggle/TopActivity.java
@@ -20,6 +20,7 @@ import android.widget.TabHost;
 import com.flurry.android.FlurryAgent;
 import com.geeksville.android.GeeksvilleExceptionHandler;
 import com.geeksville.android.PostMortemReportExceptionHandler;
+import com.geeksville.billing.Donate;
 import com.geeksville.info.Units;
 import com.geeksville.location.LocationLogDbAdapter;
 import com.geeksville.view.AsyncProgressDialog;
@@ -30,210 +31,231 @@ import com.geeksville.view.AsyncProgressDialog;
  */
 public class TopActivity extends TabActivity {
 
-	/**
-	 * Debugging tag
-	 */
-	private static final String TAG = "TopActivity";
+  /**
+   * Debugging tag
+   */
+  private static final String TAG = "TopActivity";
 
-	protected PostMortemReportExceptionHandler damageReport = new GeeksvilleExceptionHandler(
-			this);
+  protected PostMortemReportExceptionHandler damageReport = new GeeksvilleExceptionHandler(
+      this);
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-		damageReport.perhapsInstall();
+    damageReport.perhapsInstall();
 
-		// FIXME, localize these strings
-		TabHost tabHost = getTabHost();
+    // FIXME, localize these strings
+    TabHost tabHost = getTabHost();
 
-		TabHost.TabSpec spec = tabHost.newTabSpec("FlightInst");
-		// FIXME - need real art
-		spec.setIndicator(null, getResources().getDrawable(R.drawable.icon));
-		spec.setContent(new Intent(this, LoggingControl.class));
-		tabHost.addTab(spec);
+    TabHost.TabSpec spec = tabHost.newTabSpec("FlightInst");
+    // FIXME - need real art
+    spec.setIndicator(null, getResources().getDrawable(R.drawable.icon));
+    spec.setContent(new Intent(this, LoggingControl.class));
+    tabHost.addTab(spec);
 
-		spec = tabHost.newTabSpec("Logs");
-		spec.setIndicator(null, getResources().getDrawable(android.R.drawable.ic_menu_slideshow));
-		spec.setContent(new Intent(this, ListFlightsActivity.class));
-		tabHost.addTab(spec);
+    spec = tabHost.newTabSpec("Logs");
+    spec.setIndicator(null,
+        getResources().getDrawable(android.R.drawable.ic_menu_slideshow));
+    spec.setContent(new Intent(this, ListFlightsActivity.class));
+    tabHost.addTab(spec);
 
-		spec = tabHost.newTabSpec("Waypoints");
-		spec.setIndicator(null, getResources().getDrawable(android.R.drawable.ic_menu_myplaces));
-		spec.setContent(new Intent(this, ListWaypointsActivity.class));
-		tabHost.addTab(spec);
+    spec = tabHost.newTabSpec("Waypoints");
+    spec.setIndicator(null,
+        getResources().getDrawable(android.R.drawable.ic_menu_myplaces));
+    spec.setContent(new Intent(this, ListWaypointsActivity.class));
+    tabHost.addTab(spec);
 
-		spec = tabHost.newTabSpec("LiveMap");
-		spec.setIndicator(null, getResources().getDrawable(android.R.drawable.ic_menu_mapmode));
-		spec.setContent(FlyMapActivity.createIntentLive(this));
-		tabHost.addTab(spec);
-	}
+    spec = tabHost.newTabSpec("LiveMap");
+    spec.setIndicator(null,
+        getResources().getDrawable(android.R.drawable.ic_menu_mapmode));
+    spec.setContent(FlyMapActivity.createIntentLive(this));
+    tabHost.addTab(spec);
+  }
 
-	/**
-	 * Do we have any flights in our logbook?
-	 * 
-	 * @return
-	 */
-	private boolean isFlightsLogged() {
-		LocationLogDbAdapter db = new LocationLogDbAdapter(this);
-		Cursor flights = db.fetchAllFlights();
+  /**
+   * Do we have any flights in our logbook?
+   * 
+   * @return
+   */
+  private boolean isFlightsLogged() {
+    LocationLogDbAdapter db = new LocationLogDbAdapter(this);
+    Cursor flights = db.fetchAllFlights();
 
-		int numflights = flights.getCount();
+    int numflights = flights.getCount();
 
-		flights.close();
-		db.close();
+    flights.close();
+    db.close();
 
-		return numflights != 0;
-	}
+    return numflights != 0;
+  }
 
-	/**
-	 * Collect app metrics on Flurry
-	 * 
-	 * @see android.app.Activity#onStart()
-	 */
-	@Override
-	protected void onStart() {
-		Log.d(TAG, "onStart() called");
+  /**
+   * Collect app metrics on Flurry
+   * 
+   * @see android.app.Activity#onStart()
+   */
+  @Override
+  protected void onStart() {
+    Log.d(TAG, "onStart() called");
 
-		super.onStart();
+    super.onStart();
 
-		FlurryAgent.onStartSession(this, "XBPNNCR4T72PEBX17GKF");
+    FlurryAgent.onStartSession(this, "XBPNNCR4T72PEBX17GKF");
 
-		BetaSplashActivity.perhapsSplash(this);
+    BetaSplashActivity.perhapsSplash(this);
+    // Doesn't yet work - problems with google market
+    // Donate d = new Donate(this);
+    // d.perhapsSplash();
 
-		updateFromOld();
+    updateFromOld();
 
-		if (!isFlightsLogged()) {
-			getTabHost().setCurrentTabByTag("Logs");
-		}
-	}
+    if (!isFlightsLogged()) {
+      getTabHost().setCurrentTabByTag("Logs");
+    }
+  }
 
-	private static final int MIN_GOOD_VERSION = 1;
+  private static final int MIN_GOOD_VERSION = 1;
 
-	/**
-	 * Check to see if the app has all the updates needed to the filesystem
-	 * (using a build #)
-	 */
-	private void updateFromOld() {
-		final SharedPreferences prefs = getPreferences(MODE_PRIVATE);
-		int oldVer = prefs.getInt("prefsVer", 0);
+  /**
+   * Check to see if the app has all the updates needed to the filesystem (using
+   * a build #)
+   */
+  private void updateFromOld() {
+    final SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+    int oldVer = prefs.getInt("prefsVer", 0);
 
-		if (oldVer < MIN_GOOD_VERSION) {
-			AsyncProgressDialog progress = new AsyncProgressDialog(this,
-					getString(R.string.updating_from_a_prior_release),
-					getString(R.string.please_wait)) {
+    if (oldVer < MIN_GOOD_VERSION) {
+      AsyncProgressDialog progress = new AsyncProgressDialog(this,
+          getString(R.string.updating_from_a_prior_release),
+          getString(R.string.please_wait)) {
 
-				@Override
-				protected void doInBackground() {
-					wipeBadOSMTiles();
-					prefs.edit().putInt("prefsVer", MIN_GOOD_VERSION).commit();
-				}
+        @Override
+        protected void doInBackground() {
+          wipeBadOSMTiles();
+          prefs.edit().putInt("prefsVer", MIN_GOOD_VERSION).commit();
+        }
 
-			};
-			progress.execute();
-		}
-	}
+      };
+      progress.execute();
+    }
+  }
 
-	/**
-	 * There was a bad OSM load that used suffixes that confused the image
-	 * library
-	 */
-	private void wipeBadOSMTiles() {
-		File dir = OpenStreetMapTileProviderConstants.TILE_PATH_BASE;
+  /**
+   * There was a bad OSM load that used suffixes that confused the image library
+   */
+  private void wipeBadOSMTiles() {
+    File dir = OpenStreetMapTileProviderConstants.TILE_PATH_BASE;
 
-		deleteAllImages(dir);
-	}
+    deleteAllImages(dir);
+  }
 
-	/**
-	 * Recursively delete all image files in a directory
-	 * 
-	 * @param dir
-	 */
-	private void deleteAllImages(File dir) {
+  /**
+   * Recursively delete all image files in a directory
+   * 
+   * @param dir
+   */
+  private void deleteAllImages(File dir) {
 
-		if (dir.isDirectory()) {
-			File[] children = dir.listFiles();
+    if (dir.isDirectory()) {
+      File[] children = dir.listFiles();
 
-			for (File f : children)
-				deleteAllImages(f);
-		} else {
-			String filename = dir.getName().toLowerCase();
+      for (File f : children)
+        deleteAllImages(f);
+    } else {
+      String filename = dir.getName().toLowerCase();
 
-			if (filename.endsWith(".jpg") || filename.endsWith(".png")) {
-				Log.d(TAG, "Deleting " + dir);
-				dir.delete();
-			}
-		}
-	}
+      if (filename.endsWith(".jpg") || filename.endsWith(".png")) {
+        Log.d(TAG, "Deleting " + dir);
+        dir.delete();
+      }
+    }
+  }
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		super.onActivityResult(requestCode, resultCode, intent);
-		if (intent != null) {
-			Bundle extras = intent.getExtras();
-		}
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    super.onActivityResult(requestCode, resultCode, intent);
+    if (intent != null) {
+      Bundle extras = intent.getExtras();
+    }
 
-		BetaSplashActivity.handleActivityResult(this, requestCode, resultCode);
-	}
+    BetaSplashActivity.handleActivityResult(this, requestCode, resultCode);
+  }
 
-	/**
-	 * Collect app metrics on Flurry
-	 * 
-	 * @see android.app.Activity#onStop()
-	 */
-	@Override
-	protected void onStop() {
-		Log.d(TAG, "onStop() called");
+  /**
+   * Collect app metrics on Flurry
+   * 
+   * @see android.app.Activity#onStop()
+   */
+  @Override
+  protected void onStop() {
+    Log.d(TAG, "onStop() called");
 
-		super.onStop();
+    super.onStop();
 
-		FlurryAgent.onEndSession(this);
-	}
+    FlurryAgent.onEndSession(this);
+  }
 
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
 
-		// If we are leaving, we don't need the service no more
-		((GaggleApplication) getApplication()).stopGPSClient();
-	}
+    // If we are leaving, we don't need the service no more
+    ((GaggleApplication) getApplication()).stopGPSClient();
+  }
 
-	/**
-	 * @see android.app.ActivityGroup#onPause()
-	 */
-	@Override
-	protected void onResume() {
-		super.onResume();
+  /**
+   * @see android.app.ActivityGroup#onPause()
+   */
+  @Override
+  protected void onResume() {
+    super.onResume();
 
-		Units.instance.setFromPrefs(this); // This should take care of making
-		// units changes work for all our
-		// subviews, I
-		// can probably remove the other calls (FIXME)
+    Units.instance.setFromPrefs(this); // This should take care of making
+    // units changes work for all our
+    // subviews, I
+    // can probably remove the other calls (FIXME)
 
-		// Figure out if the user wants to force the screen on (FIXME, only
-		// force on while flying)
-		TabHost tabHost = getTabHost();
-		GagglePrefs prefs = new GagglePrefs(this);
-		tabHost.setKeepScreenOn(prefs.isKeepScreenOn());
-	}
+    // Figure out if the user wants to force the screen on (FIXME, only
+    // force on while flying)
+    TabHost tabHost = getTabHost();
+    GagglePrefs prefs = new GagglePrefs(this);
+    tabHost.setKeepScreenOn(prefs.isKeepScreenOn());
+  }
 
-	/**
-	 * Create our options menu
-	 * 
-	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
-	 */
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
+  /**
+   * Create our options menu
+   * 
+   * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+   */
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
 
-		getMenuInflater().inflate(R.menu.shared_options, menu);
+    getMenuInflater().inflate(R.menu.shared_options, menu);
 
-		MenuItem menuItem = menu.findItem(R.id.preferences_menu);
-		menuItem.setIntent(new Intent(this, MyPreferences.class));
+    MenuItem menuItem = menu.findItem(R.id.preferences_menu);
+    menuItem.setIntent(new Intent(this, MyPreferences.class));
 
-		menuItem = menu.findItem(R.id.about_menu);
-		menuItem.setIntent(new Intent(this, AboutActivity.class));
+    menuItem = menu.findItem(R.id.about_menu);
+    menuItem.setIntent(new Intent(this, AboutActivity.class));
 
-		return true;
-	}
+    return true;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+   */
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+    case R.id.donate_menu:
+      Donate d = new Donate(this);
+      d.splash();
+      return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
 }
