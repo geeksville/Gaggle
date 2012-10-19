16c9e25ec9bdf4a88533ffde96ab25fd3514daad
Chanoch <email@chanoch.com>
2012-10-19 16:55:19 +0100

    Added copright notice to java classes without it.

--- src/com/geeksville/location/FlynetBarometerClient.java
+++ src/com/geeksville/location/FlynetBarometerClient.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * Gaggle is Copyright 2010 by Geeksville Industries LLC, a California limited liability corporation. 
+ * 
+ * Gaggle is distributed under a dual license.  We've chosen this approach because within Gaggle we've used a number
+ * of components that Geeksville Industries LLC might reuse for commercial products.  Gaggle can be distributed under
+ * either of the two licenses listed below.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. 
+ * 
+ * Commercial Distribution License
+ * If you would like to distribute Gaggle (or portions thereof) under a license other than 
+ * the "GNU General Public License, version 2", contact Geeksville Industries.  Geeksville Industries reserves
+ * the right to release Gaggle source code under a commercial license of its choice.
+ * 
+ * GNU Public License, version 2
+ * All other distribution of Gaggle must conform to the terms of the GNU Public License, version 2.  The full
+ * text of this license is included in the Gaggle source, see assets/manual/gpl-2.0.txt.
+ ******************************************************************************/
 package com.geeksville.location;
 
 import java.io.BufferedReader;
@@ -85,6 +105,14 @@ public class FlynetBarometerClient extends Observable implements
     return found != null;
   }
 
+  /**
+   * Check device exists and is enabled.
+   * 
+   * - Is there a message elsewhere to say "Have you turned the device on?"
+   * - Is the option shown disabled or does it only appear if the device is found?
+   * 
+   * @return
+   */
   private static BluetoothDevice findDevice() {
     BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
     if (adapter != null && adapter.isEnabled()) {
@@ -102,10 +130,11 @@ public class FlynetBarometerClient extends Observable implements
     return null;
   }
 
+  /**
+   * Set the altitudes in meters above sea level
+   */
   @Override
   public void setAltitude(float meters) {
-    // float p0 = 1013.25f; // Pressure at sea level (hPa)
-    // float p = p0 * (float) Math.pow((1 - meters / 44330), 5.255);
     float p0 = pressure / (float) Math.pow((1 - meters / 44330), 5.255);
 
     reference = p0;
--- src/com/geeksville/location/GPSClient.java
+++ src/com/geeksville/location/GPSClient.java
@@ -1,6 +1,23 @@
-/**
+/*******************************************************************************
+ * Gaggle is Copyright 2010 by Geeksville Industries LLC, a California limited liability corporation. 
  * 
- */
+ * Gaggle is distributed under a dual license.  We've chosen this approach because within Gaggle we've used a number
+ * of components that Geeksville Industries LLC might reuse for commercial products.  Gaggle can be distributed under
+ * either of the two licenses listed below.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details. 
+ * 
+ * Commercial Distribution License
+ * If you would like to distribute Gaggle (or portions thereof) under a license other than 
+ * the "GNU General Public License, version 2", contact Geeksville Industries.  Geeksville Industries reserves
+ * the right to release Gaggle source code under a commercial license of its choice.
+ * 
+ * GNU Public License, version 2
+ * All other distribution of Gaggle must conform to the terms of the GNU Public License, version 2.  The full
+ * text of this license is included in the Gaggle source, see assets/manual/gpl-2.0.txt.
+ ******************************************************************************/
 package com.geeksville.location;
 
 import java.lang.reflect.Method;
@@ -51,6 +68,7 @@ public class GPSClient extends Service implements IGPSClient {
    */
   private static final String TAG = "GPSClient";
 
+  // private inner class
   private MyBinder binder = new MyBinder();
 
   private HandlerThread thread = new HandlerThread("GPSClient");
@@ -567,7 +585,7 @@ public class GPSClient extends Service implements IGPSClient {
           baro.setAltitude((float) location.getAltitude());
         }
 
-        // Before forwarding the location to others, substitude the
+        // Before forwarding the location to others, substitute the
         // (better) baro based altitude
         baro.improveLocation(location);
       }
