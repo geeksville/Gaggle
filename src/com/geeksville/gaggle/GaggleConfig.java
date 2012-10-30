3a54bede70f5c0dfe8c11bd237cae2064eb06e63
Chanoch <email@chanoch.com>
2012-10-30 11:33:08 +0000

    Updated documentation of GaggleConfig.java

--- src/com/geeksville/gaggle/GaggleConfig.java
+++ src/com/geeksville/gaggle/GaggleConfig.java
@@ -1,12 +1,36 @@
 package com.geeksville.gaggle;
 
+/**
+ * A class to support testing - e.g. by disabling the barometer for testing GPS altitude calculations.
+ * 
+ * @author chanoch
+ *
+ */
 public class GaggleConfig {
+	/**
+	 * Value which determines whether to use the barometer when available.
+	 * 
+	 * Set to false in order to test your device without using the barometer even when the device has
+	 * one.
+	 * 
+	 * This won't make your device use a barometer if there isn't one on it.
+	 */
 	private static boolean useBarometer = true;
 
+	/**
+	 * Use the built in barometer (if available?)
+	 * 
+	 * @return
+	 */
 	public static boolean isUseBarometer() {
 		return useBarometer;
 	}
 
+	/**
+	 * Change the value of the use barometer when available config programatically
+	 * 
+	 * @param useBarometer
+	 */
 	public static void setUseBarometer(boolean useBarometer) {
 		GaggleConfig.useBarometer = useBarometer;
 	}
