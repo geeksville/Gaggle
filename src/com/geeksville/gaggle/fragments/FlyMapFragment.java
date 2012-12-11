5f51ed8c6b1082c1d2027653c5515704f4992472
Marc <dkm@kataplop.net>
2012-12-11 19:37:00 +0100

    overwrite "old" tracklog class by new one (from hans)

--- src/com/geeksville/gaggle/fragments/FlyMapFragment.java
+++ src/com/geeksville/gaggle/fragments/FlyMapFragment.java
@@ -22,7 +22,7 @@ import com.geeksville.location.Waypoint;
 import com.geeksville.maps.CenteredMyLocationOverlay;
 import com.geeksville.maps.PolygonOverlay;
 import com.geeksville.maps.TracklogOverlay;
-import com.geeksville.maps.TracklogOverlay2;
+import com.geeksville.maps.TracklogOverlay;
 import com.geeksville.maps.WaypointOverlay;
 import com.geeksville.weather.overlay.WeatherStationsOverlay;
 
@@ -326,7 +326,7 @@ public class FlyMapFragment  extends AbstractGeeksvilleMapFragment
 	/**
 	 * FIXME, move all the live map stuff into a subclass?
 	 */
-	private TracklogOverlay2 liveTracklogOverlay;
+	private TracklogOverlay liveTracklogOverlay;
 
 	@Override
 	public void onResume() {
@@ -347,7 +347,7 @@ public class FlyMapFragment  extends AbstractGeeksvilleMapFragment
 			liveList.addObserver(this);
 
 			altitudeView.setLocs(locs);
-			liveTracklogOverlay = new TracklogOverlay2(getActivity(), locs);
+			liveTracklogOverlay = new TracklogOverlay(getActivity(), locs);
 			mapView.getOverlays().add(liveTracklogOverlay);
 		} else {
 			perhapsAddExtraTracklog();
@@ -361,10 +361,10 @@ public class FlyMapFragment  extends AbstractGeeksvilleMapFragment
 	 * 
 	 * @param locBundle
 	 */
-	private TracklogOverlay2 createTracklogOverlay(final LocationList locs) {
+	private TracklogOverlay createTracklogOverlay(final LocationList locs) {
 		Log.d(TAG, "create Track log");
 		// Show a tracklog
-		TracklogOverlay2 tlog = new TracklogOverlay2(getActivity(), locs);
+		TracklogOverlay tlog = new TracklogOverlay(getActivity(), locs);
 
 		// Center on the tracklog (we do this in a callback, because OSM only
 		// works after the view has been layed out
--- src/com/geeksville/maps/TracklogOverlay.java
+++ src/com/geeksville/maps/TracklogOverlay.java
@@ -3,6 +3,9 @@
  */
 package com.geeksville.maps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.MapView.Projection;
@@ -13,7 +16,9 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.graphics.Path;
 import android.graphics.Point;
+import android.graphics.Rect;
 
 import com.geeksville.location.LocationList;
 
@@ -27,154 +32,162 @@ public class TracklogOverlay extends PathOverlay {
 
 	LocationList tracklog;
 
-	/**
-	 * Max up/down millimeter/sec we've seen so far
-	 */
-	int maxMMeterSec, minMMeterSec;
+  public final int DETAIL_THRESHOLD = 1000; //the last n points that are drawn with full color detail 
 
 	/**
-	 * The color we use for drawing our tracklog
+	 * Max up/down millimeter/sec -- FIXME: set according to vario limits
 	 */
-	Paint trackPaint = new Paint();
+	float maxMMeterSec = 5.0f, 
+	      minMMeterSec = -5.0f;
 
 	/**
-	 * 256 colors, 0th position is 'most down' and 255th position is 'most up'
+	 * The colors we use for drawing our tracklog
 	 */
-	int[] hsvMap = new int[256];
+	Paint[] trackPaints = new Paint[256];
+	private ArrayList<Paint> mColors;
 
+  static final int POINT_BUFFER_SIZE = 256; //should be a multiple of 4
+  private float[] mPointBuffer = new float[4];
+  private final Point mTempPoint1 = new Point();
+  private final Point mTempPoint2 = new Point();
+  private final Path mPath = new Path();
+private List<Point> mPoints = new ArrayList<Point>();
+  
 	public TracklogOverlay(Context ctx, LocationList locs) {
 		super(Color.RED, ctx);
 
 		tracklog = locs;
 
-		trackPaint.setStyle(Style.STROKE);
-		trackPaint.setStrokeWidth(2);
-
-		calcHSVMap();
+		precalcTrackPaints();
 	}
 
-	private void calcHSVMap() {
-		float[] hsv = { 1.0f, 1.0f, 1.0f };
-
-		for (int i = 0; i < hsvMap.length; i++) {
-			hsv[0] = (float) i * 360 / hsvMap.length;
-
-			hsvMap[i] = Color.HSVToColor(hsv);
-		}
+	private void precalcTrackPaints() {
+	  for (int i=0;i<256;i++) {
+	    Paint trackPaint = new Paint();
+	    trackPaint.setColor(Color.rgb(255-Math.abs(127-i)*2, i, 255-i));  // blue -> red -> green
+      trackPaint.setStyle(Style.STROKE);
+      trackPaint.setStrokeWidth(2);
+      trackPaints[i]=trackPaint;
+	  }
 	}
 
-	/**
-	 * Draw a red line for our tracklog
-	 * 
-	 * @see Overlay#draw(android.graphics.Canvas, boolean)
-	 */
-	public void fixmeBustedOnOSM(Canvas canvas, MapView mapView) {
+  @Override
+  public void clearPath()
+  {
+      super.clearPath();
+      this.mColors = new ArrayList<Paint>();
+  }
 
-		Projection proj = mapView.getProjection();
+  /**
+   * This method draws the line.
+   * Note - highly optimized to handle long paths, proceed with care. Should be fine up to 10K points.
+   */
+  @Override
+  protected void draw(final Canvas canvas, final MapView mapView, final boolean shadow) 
+  {
+                if (this.tracklog.numPoints() < 2)
+          {
+                  //nothing to paint
+                  return;
+          }
 
-		Point p = new Point();
-		int prevZ = 0, prevTime = 0;
-		float prevX = 0, prevY = 0;
-		boolean hasPrev = false;
+          final Projection pj = mapView.getProjection();
+          
+          int size = this.tracklog.numPoints();
+          if (size > mPoints.size()) {
+            for (int i=getNumberOfPoints(); i<size; i++) {
+              GeoPoint gpt = this.tracklog.getGeoPoint(i);
+              Point pt = new Point(gpt.getLatitudeE6(), gpt.getLongitudeE6());
+              pj.toMapPixelsProjected(pt.x, pt.y, pt); //convert to map projection
+              mPoints.add(pt);
+              if (i>0) {
+                float rise = (tracklog.getAltitudeMM(i) - tracklog.getAltitudeMM(i-1)) / (tracklog.getTimeMsec(i) - tracklog.getTimeMsec(i-1)); // mm/ms = m/s
+                rise       = Math.min(Math.max(rise,minMMeterSec),maxMMeterSec); // bound to limits
+                float level  = (rise - minMMeterSec) / (maxMMeterSec - minMMeterSec) * 255.f;
+                mColors.add(trackPaints[(int)level]);
+              } else
+                mColors.add(trackPaints[127]); 
+            }
+          }
+                            
+          Point screenPoint0 = null; //points on screen
+          Point screenPoint1 = null;
+          Point projectedPoint0; //points from the points list
+          Point projectedPoint1;
+          
+          float [] buffer = this.mPointBuffer;
+          int bufferCount = 0;            
+          Rect clipBounds = pj.fromPixelsToProjected(canvas.getClipBounds()); // clipping rectangle in the intermediate projection, to avoid performing projection.
+          Rect lineBounds = new Rect(); // bounding rectangle for the current line segment.
+          
+          mPath.rewind();
+          projectedPoint0 = this.mPoints.get(size - 1);           
+          lineBounds.set(projectedPoint0.x, projectedPoint0.y, projectedPoint0.x, projectedPoint0.y);
 
-		int[] times = tracklog.timeMsec.toUnsafeArray();
-		int[] alts = tracklog.altitudeMM.toUnsafeArray();
+          boolean movedToStart = false;
+          for(int i = size - 2; i >= 0; i--)
+          {
+                  //compute next points
+                  projectedPoint1 = this.mPoints.get(i);
+                  lineBounds.union(projectedPoint1.x, projectedPoint1.y);
+                  
+                  if (!Rect.intersects(clipBounds, lineBounds))
+                  {
+                          //skip this line, move to next point
+                          projectedPoint0 = projectedPoint1;                              
+                          screenPoint0 = null;
+                          continue;
+                  }
+                  
+                  // the starting point may be not calculated, because previous segment was out of clip bounds                    
+                  if (screenPoint0 == null)
+                  {
+                          screenPoint0 = pj.toMapPixelsTranslated(projectedPoint0, this.mTempPoint1);
+                  }
+                                          
+                  screenPoint1 = pj.toMapPixelsTranslated(projectedPoint1, this.mTempPoint2);
+                  
+                  //skip this point, too close to previous point
+                  if (Math.abs(screenPoint1.x - screenPoint0.x) + Math.abs(screenPoint1.y - screenPoint0.y) <= 1)
+                  {
+                          continue;
+                  }
+                  
+                  //add new line to buffer                        
+                  
+                  if (i > size - DETAIL_THRESHOLD) {                // if we are in the detailed range 
+                      buffer[0] = screenPoint0.x;
+                      buffer[1] = screenPoint0.y;
+                      buffer[2] = screenPoint1.x;
+                      buffer[3] = screenPoint1.y;
+//                      bufferCount += 4;
+                    canvas.drawLines(buffer, this.mColors.get(i));  // paint with accurate colors
+//                    bufferCount = 0;
+                  } else {
+                    // path draws a lot faster than lines with supplied paint, so for the bulk-lines that are all red, we use a path:
+                      if(!movedToStart){
+                          movedToStart = true;
+                          mPath.moveTo(screenPoint0.x, screenPoint0.y);
+                      }
+                      mPath.lineTo(screenPoint1.x, screenPoint1.y);
+                  }
+//                  if (bufferCount == POINT_BUFFER_SIZE) {           // else just paint in red
+//                    canvas.drawLines(buffer, this.trackPaints[127]);
+//                    bufferCount = 0;
+//                  }                               
+                  //update starting point to next position 
+                  projectedPoint0 = projectedPoint1;                      
+                  screenPoint0.x = screenPoint1.x;
+                  screenPoint0.y = screenPoint1.y;                        
+                  lineBounds.set(projectedPoint0.x, projectedPoint0.y, projectedPoint0.x, projectedPoint0.y);                     
+          }
+          canvas.drawPath(mPath, this.trackPaints[127]);
 
-		// Provide rational initial condition
-		if (tracklog.numPoints() > 0) {
-			prevZ = alts[0];
-			prevTime = times[0];
-		}
-
-		int hue = hsvMap.length / 2; // Default to something middling
-		for (int i = 0; i < tracklog.numPoints(); i++) {
-			GeoPoint gp = tracklog.getGeoPoint(i);
-			int curTime = times[i];
-			// for time
-			// FIXME - busted on OSM
-			// proj.toPixels(gp, p);
-
-			if (hasPrev) {
-				int deltaMsec = curTime - prevTime;
-
-				// Integrate over the last X seconds of time
-				int integrateMsec = 10 * 1000;
-
-				if (deltaMsec > integrateMsec) {
-					int curZ = alts[i];
-					int deltaMM = curZ - prevZ;
-					int mmPerSecUp = (deltaMM * 1000) / deltaMsec;
-
-					if (mmPerSecUp > maxMMeterSec)
-						maxMMeterSec = mmPerSecUp;
-
-					if (mmPerSecUp < minMMeterSec)
-						minMMeterSec = mmPerSecUp;
-
-					// linear interpolate between min and max vert speed, 1.0==
-					// max
-					hue = (hsvMap.length - 1) * (mmPerSecUp - minMMeterSec)
-							/ (maxMMeterSec - minMMeterSec);
-
-					// Scale hue so max up is yellow, and max down is blue
-					if (hue >= hsvMap.length)
-						hue = hsvMap.length - 1;
-					else if (hue < 0)
-						hue = 0;
-
-					prevTime = curTime;
-					prevZ = curZ;
-					// FIXME, we should do some sort of moving boxcar average
-					// instead
-				}
-
-				// trackPaint.setARGB(255, 255, 0, 0);
-				int color = hsvMap[hue];
-
-				trackPaint.setColor(color);
-
-				// FIXME - for live flights we should only plot a detailed
-				// tracklog for the last
-				// 20 minutes. Prior to that we
-				// can skip points
-
-				// FIXME, the array based versions are probably faster
-				canvas.drawLine(prevX, prevY, p.x, p.y, trackPaint);
-			}
-
-			prevX = p.x;
-			prevY = p.y;
-			hasPrev = true;
-		}
-	}
-
-	/**
-	 * Until I can customize the OSM code, convert to their format
-	 */
-	private void convertToOSM() {
-		int mycount = tracklog.numPoints();
-		int osmCount = getNumberOfPoints();
-
-		if (mycount < osmCount) {
-			clearPath();
-			osmCount = 0;
-		}
-
-		for (int i = osmCount; i < mycount; i++)
-			addPoint(tracklog.getGeoPoint(i));
-	}
-
-	/**
-	 * Convert our points into OSM format then call their drawer
-	 * 
-	 * @see org.andnav.osm.views.overlay.OpenStreetMapViewPathOverlay#onDraw(android.graphics.Canvas,
-	 *      org.andnav.osm.views.OpenStreetMapView)
-	 */
-	@Override
-	protected void draw(Canvas canvas, MapView mapView, boolean shadow) {
-		convertToOSM();
-
-		super.draw(canvas, mapView, shadow);
-	}
+//          if (bufferCount >0 )
+//          {
+//                  canvas.drawLines(buffer, 0,  bufferCount, this.mPaint);
+//          }
+  }
+	
 
 }
